5a2ae3482e6a02b4a96a878b907bdec075371103
pamudu <pamuduprabathiya28@gmail.com>
2025-02-28 16:15:48 +0530

    Implement Project setup

---
+++ src/main/java/com/weatherapp/weather_analytics_server/WeatherAnalyticsServerApplication.java
@@ -0,0 +1,13 @@
+package com.weatherapp.weather_analytics_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherAnalyticsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherAnalyticsServerApplication.class, args);
+	}
+
+}
